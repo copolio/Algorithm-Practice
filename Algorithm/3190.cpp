5f12ef66c87826a311cb27e49ccdae79c5562ec8
Kijung Kim <rlwjd1504@gmail.com>
2021-01-28 19:06:40 +0900

    Solved : Baekjun - 13458

--- Algorithm/3190.cpp
+++ Algorithm/3190.cpp
@@ -60,65 +60,65 @@ void PrintMap(int n) {
 	cout << endl;
 }
 
-int main() {
-	int N, K, L;
-	int time = 0;
-	vector<Point> snake (1, Point());
-	map<int, char> snakeDirections;
-	gamemap[0][0] = 2;
-
-	cin >> N >> K;
-	for (int i = 0; i < K; i++) {
-		int col, row;
-		cin >> col >> row;
-		gamemap[col - 1][row - 1] = 1;
-	}
-	cin >> L;
-	for (int i = 0; i < L; i++) {
-		int X;
-		char C;
-		cin >> X >> C;
-		snakeDirections[X] = C;
-	}
-
-	while (true) {
-		//cout << "time : " << time << endl;
-		//PrintMap(N);
-		Point head = snake[0];
-		if (snakeDirections.count(time) > 0) {
-			if (snakeDirections[time] == 'L') {
-				head.dir.CounterRotate();
-			}
-			else if (snakeDirections[time] == 'D') {
-				head.dir.Rotate();
-			}
-		}
-		head.Move();
-		time++;
-		//Á×¾úÀ»¶§;
-		if (head.x < 0 || head.x >= N || head.y < 0 || head.y >= N) {
-			cout << time << endl;
-			break;
-		}
-		if (gamemap[head.y][head.x] == 0) {
-			for (auto point : snake) {
-				gamemap[point.y][point.x] = 0;
-			}
-			snake.insert(snake.begin(), head);
-			snake.pop_back();
-			for (auto point : snake) {
-				gamemap[point.y][point.x] = 2;
-			}
-		}
-		else if (gamemap[head.y][head.x] == 1) {
-			gamemap[head.y][head.x] = 2;
-			snake.insert(snake.begin(), head);
-		}
-		else if (gamemap[head.y][head.x] == 2) {
-			cout << time << endl;
-			break;
-		}
-	}
-
-	return 0;
-}
+//int main() {
+//	int N, K, L;
+//	int time = 0;
+//	vector<Point> snake (1, Point());
+//	map<int, char> snakeDirections;
+//	gamemap[0][0] = 2;
+//
+//	cin >> N >> K;
+//	for (int i = 0; i < K; i++) {
+//		int col, row;
+//		cin >> col >> row;
+//		gamemap[col - 1][row - 1] = 1;
+//	}
+//	cin >> L;
+//	for (int i = 0; i < L; i++) {
+//		int X;
+//		char C;
+//		cin >> X >> C;
+//		snakeDirections[X] = C;
+//	}
+//
+//	while (true) {
+//		//cout << "time : " << time << endl;
+//		//PrintMap(N);
+//		Point head = snake[0];
+//		if (snakeDirections.count(time) > 0) {
+//			if (snakeDirections[time] == 'L') {
+//				head.dir.CounterRotate();
+//			}
+//			else if (snakeDirections[time] == 'D') {
+//				head.dir.Rotate();
+//			}
+//		}
+//		head.Move();
+//		time++;
+//		//Á×¾úÀ»¶§;
+//		if (head.x < 0 || head.x >= N || head.y < 0 || head.y >= N) {
+//			cout << time << endl;
+//			break;
+//		}
+//		if (gamemap[head.y][head.x] == 0) {
+//			for (auto point : snake) {
+//				gamemap[point.y][point.x] = 0;
+//			}
+//			snake.insert(snake.begin(), head);
+//			snake.pop_back();
+//			for (auto point : snake) {
+//				gamemap[point.y][point.x] = 2;
+//			}
+//		}
+//		else if (gamemap[head.y][head.x] == 1) {
+//			gamemap[head.y][head.x] = 2;
+//			snake.insert(snake.begin(), head);
+//		}
+//		else if (gamemap[head.y][head.x] == 2) {
+//			cout << time << endl;
+//			break;
+//		}
+//	}
+//
+//	return 0;
+//}
