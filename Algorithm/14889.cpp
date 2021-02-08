de1f3a611ae6df3a1605ce0c4d7ac475c3833938
Kijung Kim <rlwjd1504@gmail.com>
2021-02-08 11:00:31 +0900

    Solved: Baekjun 14889 (https://www.acmicpc.net/problem/14889)

---
+++ Algorithm/14889.cpp
@@ -0,0 +1,42 @@
+//#include <iostream>
+//#define INF 1e9
+//using namespace std;
+//int N, diff = INF;
+//int S[20][20] = { 0, };
+//bool used[20] = { false, };
+//void getDifference(int startIdx, int nUsed) {
+//	if (nUsed == N / 2) {
+//		int resultStart = 0;
+//		int resultLink = 0;
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < N; j++) {
+//				if (used[i] && used[j]) {
+//					resultStart += S[i][j];
+//				}
+//				else if (!used[i] && !used[j]) {
+//					resultLink += S[i][j];
+//				}
+//			}
+//		}
+//		diff = abs(resultStart - resultLink) < diff ? abs(resultStart - resultLink) : diff;
+//		return;
+//	}
+//	for (int i = startIdx; i < N; i++) {
+//		if (!used[i]) {
+//			used[i] = true;
+//			getDifference(i+1, nUsed+1);
+//			used[i] = false;
+//		}
+//	}
+//}
+//int main() {
+//	cin >> N;
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < N; j++) {
+//			cin >> S[i][j];
+//		}
+//	}
+//	getDifference(0, 0);
+//	cout << diff << endl;
+//	return 0;
+//}
