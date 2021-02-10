30418503d6f5638422251fbfa27eb7bf716f99cc
Kijung Kim <rlwjd1504@gmail.com>
2021-02-10 13:01:27 +0900

    Solved: Baekjun 15683 (https://www.acmicpc.net/problem/15683)

---
+++ Algorithm/15683.cpp
@@ -0,0 +1,322 @@
+//#include <iostream>
+//#include <cstring>
+//#include <vector>
+//#include <queue>
+//#define INF 1e9
+//
+//using namespace std;
+//
+//int N, M, minDeadzoneArea = INF;
+////vector<vector<int>> office;
+//int office[8][8];
+//vector<pair<int, int>> cameras;
+//int dirX[4] = { 0, 0, 1, -1 };
+//int dirY[4] = { 1, -1, 0, 0 };
+//
+//void GetDeadZone(int office_copy[8][8]) {
+//	int area = 0;
+//	cout << endl;
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			cout << office_copy[i][j] << " ";
+//			if (office_copy[i][j] == 0) area++;
+//		}
+//		cout << endl;
+//	}
+//	minDeadzoneArea = area < minDeadzoneArea ? area : minDeadzoneArea;
+//}
+//
+//bool EqualArray(int arr1[8][8], int arr2[8][8]) {
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			if (arr1[i][j] != arr2[i][j]) return false;
+//		}
+//	}
+//	return true;
+//}
+//
+//void DFS(int office_copy[8][8], int idx) {
+//	//cout << idx << endl;
+//	if (cameras.empty()) {
+//		GetDeadZone(office_copy);
+//		return;
+//	}
+//	if (idx >= cameras.size()) {
+//		GetDeadZone(office_copy);
+//		return;
+//	}
+//	if (office[cameras[idx].first][cameras[idx].second] == 1) {
+//		for (int i = 0; i < 4; i++) {
+//			int temp[8][8] = { 0, };
+//			memcpy(temp, office_copy, sizeof(int)*8*8);
+//			int row = cameras[idx].first;
+//			int col = cameras[idx].second;
+//			while (true) {
+//				row += dirY[i];
+//				col += dirX[i];
+//				if (row < 0 || row > N - 1 || col < 0 || col > M - 1) break;
+//				if (temp[row][col] == 6) break;
+//				if (temp[row][col] == 0) temp[row][col] = 7;
+//			}
+//			DFS(temp, idx + 1);
+//		}
+//	}
+//	else if (office[cameras[idx].first][cameras[idx].second] == 2) {
+//		for (int i = 0; i < 2; i++) {
+//			int temp[8][8] = { 0, };
+//			memcpy(temp, office_copy, sizeof(int) * 8 * 8);
+//			bool visited[8][8] = { false, };
+//			queue<pair<int, int>> q;
+//			q.push(cameras[idx]);
+//			while (!q.empty()) {
+//				int row = q.front().first;
+//				int col = q.front().second;
+//				q.pop();
+//				visited[row][col] = true;
+//				if (temp[row][col] == 0) temp[row][col] = 7;
+//				if (row + dirY[i * 2] >= 0 && 
+//					row + dirY[i * 2] < N && 
+//					col + dirX[i * 2] >= 0 && 
+//					col + dirX[i * 2] < M && 
+//					!visited[row + dirY[i * 2]][col + dirX[i * 2]] &&
+//					temp[row + dirY[i * 2]][col + dirX[i * 2]] != 6)
+//					q.push(make_pair(row + dirY[i * 2], col + dirX[i * 2]));
+//				if (row + dirY[i * 2 + 1] >= 0 && 
+//					row + dirY[i * 2 + 1] < N && 
+//					col + dirX[i * 2 + 1] >= 0 && 
+//					col + dirX[i * 2 + 1] < M && 
+//					!visited[row + dirY[i * 2 + 1]][col + dirX[i * 2 + 1]] &&
+//					temp[row + dirY[i * 2 + 1]][col + dirX[i * 2 + 1]] != 6)
+//					q.push(make_pair(row + dirY[i * 2 + 1], col + dirX[i * 2 + 1]));
+//			}
+//			DFS(temp, idx + 1);
+//		}
+//	}
+//	else if (office[cameras[idx].first][cameras[idx].second] == 3) {
+//		
+//		for (int i = 0; i < 4; i++) {
+//			int temp[8][8] = { 0, };
+//			memcpy(temp, office_copy, sizeof(int) * 8 * 8);
+//			for (int j = i-1; j <= i; j++) {
+//				int row = cameras[idx].first;
+//				int col = cameras[idx].second;
+//				while (true) {
+//					row += dirY[j];
+//					col += dirX[j];
+//					if (row < 0 || row > N - 1 || col < 0 || col > M - 1) break;
+//					if (temp[row][col] == 6) break;
+//					if (temp[row][col] == 0) temp[row][col] = 7;
+//				}
+//			}
+//			DFS(temp, idx + 1);
+//		}
+//	}
+//	else if (office[cameras[idx].first][cameras[idx].second] == 4) {
+//		for (int i = 0; i < 4; i++) {
+//			int temp[8][8] = { 0, };
+//			memcpy(temp, office_copy, sizeof(int) * 8 * 8);
+//			for (int j = 0; j < 4; j++) {
+//				if (j == i) continue;
+//				int row = cameras[idx].first;
+//				int col = cameras[idx].second;
+//				while (true) {
+//					row += dirY[j];
+//					col += dirX[j];
+//					if (row < 0 || row > N - 1 || col < 0 || col > M - 1) break;
+//					if (temp[row][col] == 6) break;
+//					if (temp[row][col] == 0) temp[row][col] = 7;
+//				}
+//			}
+//			DFS(temp, idx + 1);
+//		}
+//	}
+//	else if (office[cameras[idx].first][cameras[idx].second] == 5) {
+//		int temp[8][8] = { 0, };
+//		memcpy(temp, office_copy, sizeof(int) * 8 * 8);
+//		for (int i = 0; i < 4; i++) {
+//			int row = cameras[idx].first;
+//			int col = cameras[idx].second;
+//			while (true) {
+//				row += dirY[i];
+//				col += dirX[i];
+//				if (row < 0 || row > N - 1 || col < 0 || col > M - 1) break;
+//				if (temp[row][col] == 6) break;
+//				if (temp[row][col] == 0) temp[row][col] = 7;
+//			}
+//		}
+//		DFS(temp, idx + 1);
+//	}
+//}
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//
+//	cin >> N >> M;
+//	
+//	for (int i = 0; i < N; i++) {
+//		vector<int> row;
+//		for (int j = 0; j < M; j++) {
+//			int room;
+//			cin >> room;
+//			if (room > 0 && room < 6) {
+//				cameras.push_back(make_pair(i, j));
+//			}
+//			office[i][j] = room;
+//		}
+//	}
+//	DFS(office, 0);
+//	cout << minDeadzoneArea;
+//
+//	return 0;
+//}
+
+//////////// 이상 시간초과 코드 //////////////
+//////////// 이하 정답 코드 //////////////
+
+//#include <iostream>
+//#define endl '\n'
+//using namespace std;
+//
+//struct CCTV
+//{
+//	int type, r, c;
+//};
+//int N, M, ans = 1e9, map[8][8];
+//int cctv_size;
+//CCTV cctv[8];
+//const int rotation[] = { NULL, 4, 2, 4, 4, 1 };
+//
+//void map_cpy(int desc[8][8], int src[8][8])
+//{
+//	for (int r = 0; r < N; ++r)
+//		for (int c = 0; c < M; ++c)
+//			desc[r][c] = src[r][c];
+//}
+//// update 함수는 한 방향으로만 감시를 해 주는 함수.
+//void update(int dir, CCTV cctv)
+//{
+//	// cctv를 기준으로 dir 방향에 대한 체크를 한다.
+//	// dir = 0 : 동, 1 : 북, 2 : 서, 3 : 남
+//	dir %= 4;    // dir값이 4보다 큰 값이 들어올 수 있으므로
+//	if (dir == 0)            // 동쪽
+//	{
+//		for (int c = cctv.c + 1; c < M; ++c)
+//		{
+//			if (map[cctv.r][c] == 6) break;
+//			map[cctv.r][c] = -1;
+//		}
+//	}
+//	else if (dir == 1)    // 북쪽
+//	{
+//		for (int r = cctv.r - 1; r >= 0; --r)
+//		{
+//			if (map[r][cctv.c] == 6) break;
+//			map[r][cctv.c] = -1;
+//		}
+//	}
+//	else if (dir == 2)    // 서쪽
+//	{
+//		for (int c = cctv.c - 1; c >= 0; --c)
+//		{
+//			if (map[cctv.r][c] == 6) break;
+//			map[cctv.r][c] = -1;
+//		}
+//	}
+//	else if (dir == 3)    // 남쪽
+//	{
+//		for (int r = cctv.r + 1; r < N; ++r)
+//		{
+//			if (map[r][cctv.c] == 6) break;
+//			map[r][cctv.c] = -1;
+//		}
+//	}
+//}
+//void DFS(int cctv_idx)
+//{
+//	if (cctv_idx == cctv_size)
+//	{
+//		int cnt = 0;
+//		// 0의 개수 count
+//		for (int r = 0; r < N; ++r)
+//		{
+//			for (int c = 0; c < M; ++c)
+//			{
+//				if (map[r][c] == 0)
+//					++cnt;
+//			}
+//		}
+//		if (ans > cnt)
+//			ans = cnt;
+//		return;
+//	}
+//	int backup[8][8];
+//	int type = cctv[cctv_idx].type;
+//
+//	for (int dir = 0; dir < rotation[type]; ++dir)
+//	{
+//		// map 백업
+//		map_cpy(backup, map);
+//
+//		// map 업데이트
+//		// update 함수는 한 방향만 갱신하는 함수.
+//		// 따라서 이 함수를 필요에 맞게 호출해 주면 됨.
+//		if (type == 1)            // 1번 카메라는 한 방향 감시이니 하나만 불러줌.
+//		{
+//			update(dir, cctv[cctv_idx]);
+//		}
+//		else if (type == 2)        // 2번 카메라는 기본방향과 그 180도 반대방향을 감시함.
+//		{
+//			update(dir, cctv[cctv_idx]);
+//			update(dir + 2, cctv[cctv_idx]);
+//		}
+//		else if (type == 3)        // 3번 카메라는 기본방향과 90도 방향을 감시함.
+//		{
+//			update(dir, cctv[cctv_idx]);
+//			update(dir + 1, cctv[cctv_idx]);
+//		}
+//		else if (type == 4)        // 4번 카메라는 기본방향과 90도, 180도 방향을 감시함
+//		{
+//			update(dir, cctv[cctv_idx]);
+//			update(dir + 1, cctv[cctv_idx]);
+//			update(dir + 2, cctv[cctv_idx]);
+//		}
+//		else if (type == 5)        // 5번 카메라는 전 방향 모두 감시함
+//		{
+//			update(dir, cctv[cctv_idx]);
+//			update(dir + 1, cctv[cctv_idx]);
+//			update(dir + 2, cctv[cctv_idx]);
+//			update(dir + 3, cctv[cctv_idx]);
+//		}
+//
+//		DFS(cctv_idx + 1);
+//
+//		// map 복원 - 백트래킹
+//		map_cpy(map, backup);
+//	}
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//
+//	cin >> N >> M;
+//	for (int r = 0; r < N; ++r)
+//	{
+//		for (int c = 0; c < M; ++c)
+//		{
+//			cin >> map[r][c];
+//			if (map[r][c] != 0 && map[r][c] != 6)
+//			{
+//				cctv[cctv_size].r = r;
+//				cctv[cctv_size].c = c;
+//				cctv[cctv_size].type = map[r][c];
+//				++cctv_size;
+//			}
+//		}
+//	}
+//	DFS(0);
+//	cout << ans << endl;
+//	return 0;
+//}
