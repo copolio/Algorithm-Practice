7fac7709a0cc3defe07d33ddd8c5813c43b79310
Kijung Kim <rlwjd1504@gmail.com>
2021-02-15 17:03:07 +0900

    틀린거 리팩토링

--- Algorithm/16325.cpp
+++ Algorithm/16325.cpp
@@ -28,63 +28,70 @@ void GetInput() {
 	}
 }
 
-void Simulate() {
-	int curTime = 0;
-	while (curTime < K) {
-		//봄+여름
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				priority_queue<int, vector<int>, greater<int>> tempq;
-				while (!tree[i][j].empty()) {
-					int nReq = tree[i][j].top();
-					tree[i][j].pop();
-					//영양섭취
-					if (nutrition[i][j] >= nReq) {
-						nutrition[i][j] -= nReq;
-						tempq.push(nReq + 1);
-					}
-					//즉시사망 & 양분 추가
-					else {
-						nutrition[i][j] += nReq / 2;
-						while (!tree[i][j].empty()) {
-							nReq = tree[i][j].top();
-							tree[i][j].pop();
-							nutrition[i][j] += nReq / 2;
-						}
+void SpringSummer() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			priority_queue<int, vector<int>, greater<int>> tempq;
+			while (!tree[i][j].empty()) {
+				int age = tree[i][j].top();
+				tree[i][j].pop();
+				//영양섭취
+				if (nutrition[i][j] >= age) {
+					nutrition[i][j] -= age;
+					tempq.push(age + 1);
+				}
+				//즉시사망 & 양분 추가
+				else {
+					nutrition[i][j] += age / 2;
+					while (!tree[i][j].empty()) {
+						age = tree[i][j].top();
+						tree[i][j].pop();
+						nutrition[i][j] += age / 2;
 					}
 				}
-				tree[i][j] = tempq;
 			}
+			tree[i][j] = tempq;
 		}
-		//가을
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				priority_queue<int, vector<int>, greater<int>> tempq;
-				while (!tree[i][j].empty()) {
-					int nReq = tree[i][j].top();
-					tree[i][j].pop();
-					//번식
-					if (nReq > 0 && nReq % 5 == 0) {
-						for (int k = 0; k < 8; k++) {
-							int r = i + movedir[k][0];
-							int c = j + movedir[k][1];
-							if (r < 1 || r > N || c < 1 || c > N) continue;
-							tree[r][c].push(1);
-						}
-					}
-					tempq.push(nReq);
-				}
-				tree[i][j] = tempq;
-			}
-		}
-		//겨울
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				nutrition[i][j] += a[i][j];
-			}
-		}
+	}
+}
 
-		curTime++;
+void Fall() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (tree[i][j].empty()) continue;
+			priority_queue<int, vector<int>, greater<int>> tempq;
+			while (!tree[i][j].empty()) {
+				int age = tree[i][j].top();
+				tree[i][j].pop();
+				//번식
+				if (age > 0 && age % 5 == 0) {
+					for (int k = 0; k < 8; k++) {
+						int r = i + movedir[k][0];
+						int c = j + movedir[k][1];
+						if (r < 1 || r > N || c < 1 || c > N) continue;
+						tree[r][c].push(1);
+					}
+				}
+				tempq.push(age);
+			}
+			tree[i][j] = tempq;
+		}
+	}
+}
+
+void Winter() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			nutrition[i][j] += a[i][j];
+		}
+	}
+}
+
+void Simulate() {
+	for (int i = 0; i < K; i++) {
+		SpringSummer();
+		Fall();
+		Winter();
 	}
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
