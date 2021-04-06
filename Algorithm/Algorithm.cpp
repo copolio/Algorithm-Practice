cd8527bf1000bd24e3a93e4f9373dd977f8311c8
Kijung Kim <rlwjd1504@gmail.com>
2021-04-06 16:21:51 +0900

    Solved: actual test

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -40,11 +40,17 @@
 //	}
 //};
 //
+//#include <iostream>
+//using namespace std;
 //int main() {
-//	vector<Point> v;
-//
-//
-//	v.emplace_back(1, 2);
-//
-//	Point* point = new SpecialPoint(1, 2);
+//	int n = 5;
+//	int i, j;
+//	int x = 0;
+//	for (i = 0; i < n; i++) {
+//		for (j = 0; j <= i; j++) {
+//			x = x + j + 1;
+//		}
+//	}
+//	cout << x;
+//	return 0;
 //}
