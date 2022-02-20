f488e15abb10ef798bdf24c6611218360c49d493
Kijung Luke Kim <rlwjd1504@gmail.com>
2022-02-20 23:47:07 +0900

    푸는중

---
+++ 77486.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount)
+{
+    vector<int> answer;
+    unordered_map<string, int> profit;
+
+    for (int i = 0; i < enroll.size(); i++)
+    {
+    }
+
+    return answer;
+}
