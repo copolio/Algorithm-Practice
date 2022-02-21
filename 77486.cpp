983618e017386571378b4d5fb5ebd818635998c8
Kijung Luke Kim <rlwjd1504@gmail.com>
2022-02-21 20:43:07 +0900

    https://programmers.co.kr/learn/courses/30/lessons/77486

--- 77486.cpp
+++ 77486.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <cmath>
 #include <unordered_map>
 
 using namespace std;
@@ -7,10 +8,34 @@ using namespace std;
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount)
 {
     vector<int> answer;
-    unordered_map<string, int> profit;
+    unordered_map<string, string> tree;
+    unordered_map<string, int> sales;
 
     for (int i = 0; i < enroll.size(); i++)
     {
+        tree[enroll[i]] = referral[i];
+    }
+
+    for (int i = 0; i < seller.size(); i++)
+    {
+        int profit = amount[i] * 100;
+        string cur = seller[i];
+        while (cur != "-")
+        {
+            if (profit * 0.1 < 1)
+            {
+                sales[cur] += profit;
+                break;
+            }
+            sales[cur] += ceil(profit * 0.9);
+            profit *= 0.1;
+            cur = tree[cur];
+        }
+    }
+
+    for (int i = 0; i < enroll.size(); i++)
+    {
+        answer.push_back(sales[enroll[i]]);
     }
 
     return answer;
